290d1589dbfbf54f91cf82594e7ca8408c38416e
“midory-rojas” <midory.rojasv@gmail.com>
2025-11-18 23:12:17 +0100

    feat:aggiunto lo style alle immagine ed l'animazione in modo che quando viene cliccata possa muoversi

--- js/script.js
+++ js/script.js
@@ -4,17 +4,19 @@ const poleraidFoto = document.getElementById("poleraid-foto");
 
 axios.get("https://lanciweb.github.io/demo/api/pictures/").then(function (resp) {
     const fotoArray = resp.data;
-    //console.log(fotoArray);
+    console.log(fotoArray);
 
 
     fotoArray.forEach(function (curFoto) {
+        //console.log(curFoto);
         const colElem = document.createElement("div");
         colElem.classList.add("col");
         colElem.innerHTML = `
         <div class="card">
             <img class="spillo" src ="img/pin.svg" alt="" />
-            <img class = "foto" src ="./img/primavera img.jpg" alt="primavera" />
-            <p class="leyenda">Accusamus Beatae Ad Facilis Cum Similique Qui Sunt</p>
+            <img class = "foto" src ="${curFoto.url}" alt="primavera" />
+            <p>${curFoto.date}</p>
+            <h3>${curFoto.title}</h3>
         </div>
     `
     poleraidFoto.append(colElem);
