c99fbc1964c6b38f1ae3ef4808999731c1d0cef5
“midory-rojas” <midory.rojasv@gmail.com>
2025-11-19 11:50:23 +0100

    feat: creazione di  funzione la prima per stampare tutte le foto e l'altra per creare ogni singola foto

--- js/script.js
+++ js/script.js
@@ -1,24 +1,36 @@
-const poleraidFoto = document.getElementById("poleraid-foto");
-//console.log(poleraidFoto)
-
-
-axios.get("https://lanciweb.github.io/demo/api/pictures/").then(function (resp) {
+// Chiamata l'API con Axios
+axios
+.get("https://lanciweb.github.io/demo/api/pictures/")
+.then(function (resp) {
     const fotoArray = resp.data;
-    console.log(fotoArray);
+    //console.log(fotoArray);
+    printFoto(fotoArray);
 
+});
 
-    fotoArray.forEach(function (curFoto) {
-        //console.log(curFoto);
-        const colElem = document.createElement("div");
+// Funzione per stampare le foto
+
+function printFoto(array) {
+    const poleraidFoto = document.getElementById("poleraid-foto");
+    //console.log(poleraidFoto)
+    array.forEach(function (curFoto) {
+        const fotoElem = createOgniFoto(curFoto);
+        poleraidFoto.append(fotoElem);
+        });
+}
+
+// Funzione per creare ogni singola foto
+
+function createOgniFoto (foto) {
+    const colElem = document.createElement("div");
         colElem.classList.add("col");
         colElem.innerHTML = `
         <div class="card">
             <img class="spillo" src ="img/pin.svg" alt="" />
-            <img class = "foto" src ="${curFoto.url}" alt="primavera" />
-            <p>${curFoto.date}</p>
-            <h3>${curFoto.title}</h3>
+            <img class = "foto" src ="${foto.url}" alt="primavera" />
+            <p>${foto.date}</p>
+            <h3>${foto.title}</h3>
         </div>
-    `
-    poleraidFoto.append(colElem);
-    });
-});
+    `;
+    return colElem;
+}
